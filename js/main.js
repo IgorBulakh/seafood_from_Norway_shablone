90e825bf56a679685fcae9c474d5f0b9ccc7f82f
IgorBulakh <bulach.igor@gmail.com>
2021-12-03 19:09:05 +0300

    main block

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ $(function () {
       '<button type="button" class="slick-prev"><img src="./images/prev.svg" alt="prew"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="./images/next.svg" alt="next"></button>',
+    // vertical: true,
+    fade: true,
+  });
+
+  $(".product__name").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: ".product__content",
     vertical: true,
+
+    nextArrow:
+      '<button type="button" class="product-next"><img src="./images/product-next.svg" alt="next"></button>',
+    prevArrow:
+      '<button type="button" class="product-prev"><img src="./images/product-prev.svg" alt="prew"></button>',
+  });
+
+  $(".product__content").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".product__name",
+    fade: true,
+    arrows: false,
   });
 });
